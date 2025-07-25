1648456082b0f621afc2876c6d401105eca1378c
Ecky819 <marcoeggert73@gmail.com>
2025-07-25 20:10:31 +0200

    Diverse Änderungen und Ladebildschirm hinzugefügt

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'widgets/main_navigation.dart';
 import 'theme/app_theme.dart';
 
-void main() {
-  runApp(const MyApp());
+Future main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  // Initialisiere Flutter Launcher Icons, falls benötigt
+  FlutterNativeSplash.remove();
+  runApp(MyApp());
+}
+
+Future initialization(BuildContext? context) async {
+  // Hier können Sie Initialisierungen vornehmen, z.B. für Firebase oder andere Dienste
+  await Future.delayed(Duration(seconds: 3)); // await Firebase.initializeApp();
 }
 
 class MyApp extends StatelessWidget {
